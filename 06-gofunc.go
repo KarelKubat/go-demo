447e2ee5f635897829749190ec435ae772b8ea8e
Karel Kubat <karel@kubat.nl>
2018-11-11 00:35:14 +0100

    cosmetic

--- 06-gofunc.go
+++ 06-gofunc.go
@@ -5,13 +5,21 @@ import (
 	"time"
 )
 
-func shoutAfter(d time.Duration) {
+func testFunc(d time.Duration) {
 	time.Sleep(d)
-	fmt.Printf("shouting after %v\n", d)
+	fmt.Printf("testFunc ends after %v\n", d)
 }
 
 func main() {
-	go shoutAfter(1 * time.Second)
-	fmt.Printf("2 seconds until main() will exit\n");
-	shoutAfter(2 * time.Second)
+	// go namedFunction(args)
+	go testFunc(1 * time.Second)
+
+	// go closure(args) { .... } (parameters)
+	go func(d time.Duration) {
+		time.Sleep(d)
+		fmt.Printf("closure ends after %v\n", d);
+	}(time.Second)
+	
+	fmt.Printf("main will stop in 2 seconds\n");
+	time.Sleep(2 * time.Second)
 }
